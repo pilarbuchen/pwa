a840bf464606b8524a02509ef0d912246038c989
Pilar Buchen <pilarbuchen@gmail.com>
2021-06-27 18:13:26 -0500

    changes to manigfest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,13 @@
-const APP_PREFIX = 'budget-app';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+const STATIC_CACHE = `static-cache-v1`;
+const RUNTIME_CACHE = `runtime-cache`;
 
 const FILES_TO_CACHE = [
-    "./index.html",
-    "./css/styles.css",
-    "./js/db.js",
-    "./js/index.js",
+    "/index.html",
+    "/css/styles.css",
+    "/js/db.js",
+    "/js/index.js",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
 self.addEventListener('install', function (e) {
@@ -18,3 +19,34 @@ self.addEventListener('install', function (e) {
     )
   })
 
+// Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
+  
+  // Delete outdated caches
+  self.addEventListener('activate', function (e) {
+    e.waitUntil(
+      caches.keys().then(function (keyList) {
+        // `keyList` contains all cache names under your username.github.io
+        // filter out ones that has this app prefix to create keeplist
+        let cacheKeeplist = keyList.filter(function (key) {
+          return key.indexOf(APP_PREFIX);
+        })
+        // add current cache name to keeplist
+        cacheKeeplist.push(CACHE_NAME);
+  
+        return Promise.all(keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i] );
+            return caches.delete(keyList[i]);
+          }
+        }));
+      })
+    );
+  });
