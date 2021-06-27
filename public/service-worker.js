7190b80f353ac6c8c38312a4f48a526278a9b1a0
Pilar Buchen <pilarbuchen@gmail.com>
2021-06-27 17:32:12 -0500

    webpack

---
+++ public/service-worker.js
@@ -0,0 +1,20 @@
+const APP_PREFIX = 'budget-app';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./js/db.js",
+    "./js/index.js",
+];
+
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(function (cache) {
+        console.log('installing cache : ' + CACHE_NAME)
+        return cache.addAll(FILES_TO_CACHE)
+      })
+    )
+  })
+
